962e4c75b1fdbcb2036073435dda89944453f9e7
n2tmad <39457122+n2tmad@users.noreply.github.com>
2021-05-23 00:20:54 +0200

    WIP. small folder source changes

---
+++ backend/test/sql_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:backend/connector/item/sql/postgres/postgres_connector.dart';
+
+void main() {
+  test('sql insert', () {
+    final String insertSql = '';
+    // TODO
+  });
+}
