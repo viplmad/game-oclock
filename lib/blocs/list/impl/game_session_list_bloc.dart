8292fc97efbb51049eb51ca9a0e5541f5c9d5b6c
viplmad <39457122+viplmad@users.noreply.github.com>
2025-10-14 23:27:11 +0200

    v2 test

--- lib/blocs/list/impl/game_session_list_bloc.dart
+++ lib/blocs/list/impl/game_session_list_bloc.dart
@@ -63,10 +63,7 @@ class GameSessionListBloc extends ListLoadBloc<GameSession> {
       lastTotal: lastTotal,
     );
     return ListLoadSuccess<GameSession>(
-      data: data
-        ..sort(
-          (final a, final b) => a.compareTo(b),
-        ), // Sort to simplify computation on UI
+      data: data,
       total: count,
       quicksearch: quicksearch,
       search: search,
